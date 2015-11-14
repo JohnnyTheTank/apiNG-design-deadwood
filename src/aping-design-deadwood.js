f28bcccc2afc7a43a215f6184d7ce9d3d267cea9
Jonathan Hornung <jonathan.hornung@gmail.com>
2015-11-14 23:30:48 +0100

    impr: removed unneeded packages from bower

--- src/aping-design-deadwood.js
+++ src/aping-design-deadwood.js
@@ -1,6 +1,6 @@
 "use strict";
 
-angular.module('jtt_aping_deadwood', ['angularVideoBg', 'yaru22.angular-timeago', 'linkify'])
+angular.module('jtt_aping_deadwood', ['angularVideoBg'])
     .controller('apingDeadwoodController', ['$scope', function($scope) {
 
         $scope.$on('resultMerged', function () {
