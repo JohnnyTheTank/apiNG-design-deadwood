c7f5a1e7a26e5dc7ba9f4174f7267339f68e83f1
Jonathan Hornung <jonathan.hornung@gmail.com>
2015-11-15 15:08:44 +0100

    implemented slider

--- src/aping-design-deadwood.js
+++ src/aping-design-deadwood.js
@@ -3,9 +3,33 @@
 angular.module('jtt_aping_deadwood', ['angularVideoBg'])
     .controller('apingDeadwoodController', ['$scope', function($scope) {
 
+        //$scope.selected = {};
+
+        $scope.workingCopy = [];
+
+        //$scope.results = [];
+
         $scope.$on('resultMerged', function () {
             $scope.workingCopy = $scope.results;
+
+            if($scope.workingCopy.length>0) {
+                $scope.activateVideo($scope.workingCopy[0]);
+            }
+            setTimeout(function () {
+                $('#deadwood-owl-carousel').owlCarousel({
+                    items:4,
+                    lazyLoad:false,
+                    loop:false,
+                    margin:0
+                });
+            }, 0);
+
+
         });
+
+        $scope.activateVideo = function(_video) {
+            $scope.selected = _video;
+        }
 
     }]);
 
