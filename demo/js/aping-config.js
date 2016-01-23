58b3eae974b1ab560afcb61fac439b7d7f5468a4
Jonathan Hornung <jonathan.hornung@gmail.com>
2016-01-23 21:03:49 +0100

    small updates

--- demo/js/aping-config.js
+++ demo/js/aping-config.js
@@ -1,20 +1,13 @@
 "use strict";
 
-apingApp.config(['$provide', function ($provide) {
+angular.module('jtt_aping').config(['$provide', function ($provide) {
 
-    $provide.constant("apingApiKeys", {
-        youtube : [
-            {'apiKey':'<YOUR_YOUTUBE_API_KEY>'},
-        ]
-    });
-
-    $provide.constant("apingDefaultSettings", {
-        templateUrl : "../src/aping_design_deadwood.html",
-        items : 20, //items per request
-        maxItems: 100, //max items per aping
-        orderBy : "timestamp",
-        orderReverse : "true",
-        model: "social",
+    $provide.value("apingDefaultSettings", {
+        apingApiKeys: {
+            youtube: [
+                {'apiKey': '<YOUR_YOUTUBE_API_KEY>'}
+            ],
+        }
     });
 
 }]);
